54673b2b7e941b0bfe51f67c524dd03c193b52b4
John Morris <2e0byo@gmail.com>
2025-02-22 15:36:24 +0000

    feat: remaining features.

--- moving/__init__.py
+++ moving/__init__.py
@@ -1,13 +1,20 @@
+import asyncio
+import threading
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, Form, HTTPException, UploadFile, responses
 from jinja2 import Environment, PackageLoader, select_autoescape
 
+from moving.constants import permalink
+from moving.labels import Label
+
 from .auth import auth
-from .db import DB, CreateBox, Picture, db
+from .db import DB, Box, CreateBox, CreatePicture, db
 
 app = FastAPI()
 templates = Environment(loader=PackageLoader("moving"), autoescape=select_autoescape())
+
+label_queue = asyncio.Queue(128)
 
 
 @app.get("/")
@@ -39,23 +46,27 @@ async def add_box_submit(
         title=title,
         description=description,
         value=value,
-        interior=[await Picture.from_uploaded(pic) for pic in interior if pic.filename],
+        interior=[
+            await CreatePicture.from_uploaded(pic) for pic in interior if pic.filename
+        ],
         user=user,
     )
     box = db.add_box(create_box)
-    box_html = load_box(box.id, db)
+    box_html = render_box(box)
+    label = Label(qr_contents=permalink(f"box?id={box.id}"), no=box.id, title=box.title)
+    tex = await label.compile()
+    db.add_label(box.id, tex)
+    await label_queue.put(box.id)
     html = templates.get_template("submitted.html.jinja2").render(
         box=box, box_html=box_html
     )
     return responses.HTMLResponse(html)
 
 
-def load_box(
-    id: int,
-    db: DB,
-) -> str:
-    box = db.load_box(id)
-    return templates.get_template("box.html.jinja2").render(box=box)
+def render_box(box: Box, hide_buttons: bool = False) -> str:
+    return templates.get_template("box.html.jinja2").render(
+        box=box, hide_buttons=hide_buttons
+    )
 
 
 @app.get("/box")
@@ -65,12 +76,12 @@ def get_box(
     db: Annotated[DB, Depends(db)],
 ) -> responses.HTMLResponse:
     try:
-        box = load_box(id, db)
+        box = db.load_box(id)
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Item not found")
     else:
         return responses.HTMLResponse(
-            templates.get_template("boxes.html.jinja2").render(boxes=[box])
+            templates.get_template("boxes.html.jinja2").render(boxes=[render_box(box)])
         )
 
 
@@ -84,6 +95,94 @@ def boxes(
     return responses.HTMLResponse(
         templates.get_template("boxes.html.jinja2").render(boxes=boxes)
     )
+
+
+@app.get("/label")
+def get_label(
+    _user: Annotated[str, Depends(auth)],
+    db: Annotated[DB, Depends(db)],
+    id: int,
+) -> responses.Response:
+    try:
+        label = db.load_label(id)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Label not found")
+    else:
+        return responses.Response(label, media_type="application/pdf")
+
+
+@app.get("/image")
+def get_image(
+    _user: Annotated[str, Depends(auth)],
+    db: Annotated[DB, Depends(db)],
+    id: int,
+) -> responses.Response:
+    try:
+        img = db.load_picture(id)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Image not found")
+    else:
+        return img.as_response()
+
+
+LABEL_LOCK = threading.Lock()
+
+
+@app.get("/label-events")
+def label_events(
+    _user: Annotated[str, Depends(auth)],
+) -> responses.StreamingResponse:
+    if LABEL_LOCK.acquire(blocking=False):
+        # Note this isn't full SSE; it's just ids linewise
+        async def iter_labels():
+            while True:
+                yield f"{await label_queue.get()}\n"
+
+        return responses.StreamingResponse(
+            iter_labels(), media_type="text/event-stream"
+        )
+    else:
+        raise HTTPException(
+            status_code=409,
+            detail="Only one label stream can be active at any one time, "
+            "and one is already ongoing.",
+        )
+
+
+@app.get("/reprint")
+async def reprint(
+    _user: Annotated[str, Depends(auth)],
+    id: int,
+):
+    await label_queue.put(id)
+    template = templates.get_template("reprinted.html.jinja2")
+    return responses.HTMLResponse(template.render(id=id))
+
+
+@app.get("/delete")
+async def delete_form(
+    _user: Annotated[str, Depends(auth)],
+    id: int,
+    db: Annotated[DB, Depends(db)],
+):
+    try:
+        box = db.load_box(id)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Box not found")
+    box_html = render_box(box, hide_buttons=True)
+    template = templates.get_template("delete.html.jinja2")
+    return responses.HTMLResponse(template.render(box=box, box_html=box_html))
+
+
+@app.post("/delete")
+async def delete(
+    _user: Annotated[str, Depends(auth)],
+    id: int,
+    db: Annotated[DB, Depends(db)],
+):
+    db.delete_box(id)
+    template = templates.get_template("deleted.html.jinja2")
+    return responses.HTMLResponse(template.render(id=id))
 
 
 if __name__ == "__main__":
--- moving/constants.py
+++ moving/constants.py
@@ -1,1 +1,13 @@
+# ruff: noqa: ERA001
 SERVER_NAME = "moving.2e0byo.co.uk"
+
+
+def permalink(path: str) -> str:
+    return f"https://{SERVER_NAME}/{path}"
+
+
+# SERVER_NAME = "localhost:8000"
+
+
+# def permalink(path: str) -> str:
+#     return f"http://{SERVER_NAME}/{path}"
