9d318da309d2d4c8d1090e4010e7b740aff5c11c
John Morris <2e0byo@gmail.com>
2025-05-15 03:07:30 +0100

    feat: cli for label printing.

--- label_printer.py
+++ label_printer.py
@@ -3,12 +3,14 @@ from subprocess import run
 from tempfile import NamedTemporaryFile
 
 import httpx
+from typer import Typer
 
 from moving.auth import passwords
 from moving.constants import SERVER_NAME
 
+app = Typer()
+
 server = f"https://{SERVER_NAME}/"
-server = "http://localhost:8000/"
 
 user, password = passwords()[0]
 auth = httpx.BasicAuth(user, password)
@@ -18,6 +20,7 @@ label = server + "label?id={id}"
 PRINTER = "DYMO_LabelWriter_310"
 
 
+@app.command()
 def print_label(id: int):
     data = httpx.get(label.format(id=id), auth=auth).raise_for_status().content
     with NamedTemporaryFile(mode="wb") as f:
@@ -27,7 +30,9 @@ def print_label(id: int):
 
 
 def subscribe():
+    print("connecting")
     with httpx.stream("GET", events, auth=auth, timeout=None) as events_stream:
+        print("waiting for line")
         for line in events_stream.iter_lines():
             print("Got label", line)
             id = int(line)
@@ -37,4 +42,4 @@ def subscribe():
 
 
 if __name__ == "__main__":
-    subscribe()
+    app()
